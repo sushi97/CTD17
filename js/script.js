3a05aa56d4c76faaceedfceb4a6f1b2842f26f72
sushi97 <sushrutshimpi@gmail.com>
2017-01-19 20:20:03 +0530

    repair

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 (function ($) {
     $(function () {
 
-<<<<<<< HEAD
-
-=======
         $('.button-collapse').sideNav();
->>>>>>> refs/remotes/origin/parallax-template
         $('.parallax').parallax();
 
     }); // end of document ready
