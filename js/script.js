05b70f26d4c1cc80760ea1982506e36e086e1a78
sushi97 <sushrutshimpi@gmail.com>
2017-02-12 12:48:43 +0530

    tweaks

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@
                             opacity: '0',
                             width: '50%'
                         }, 2000, function () {
-                            $('body').css('overflow', 'visible');
+                            $('body').css('overflow', 'initial');
                             this.remove();
                         });
                         $('#page').animate({
